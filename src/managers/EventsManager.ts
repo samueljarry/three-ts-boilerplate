979e91b4fd7d8e272793fc1f977ca2226b6964e1
samueljarry <samueljarry.sj@gmail.com>
2023-10-14 15:51:38 +0200

    Feat: Functional EventsManager

--- src/managers/EventsManager.ts
+++ src/managers/EventsManager.ts
@@ -1,28 +1,47 @@
 import { EventsId } from "@constants/EventsId";
 
-type EventsSet = Set<(event?: Event) => void>
+type EventFunction = (event?: Event) => void
+type EventsSet = Set<EventFunction>
 
 export class EventsManager {
   private static _EventsMap = new Map<EventsId, EventsSet>();
   private static _EventsActionsMap = new Map<EventsId, any>();
   
   public static Init() {
-    EventsManager._EventsMap.set(EventsId.RESIZE, new Set<(event?: Event) => void>());
-    EventsManager._EventsMap.set(EventsId.KEYDOWN, new Set<(event?: Event) => void>());
-    
+    EventsManager._EventsMap.set(EventsId.RESIZE, new Set<EventFunction>());
     EventsManager._SetListeners();
   }
 
-  public static AddEventMethod(eventId: EventsId, method: (event?: Event) => void) {
+  private static _SetListeners() {
+    for(const [key, actions] of EventsManager._EventsMap) {
+      if(!EventsManager._EventsActionsMap.get(key)) {
+        const func = (event: Event) => {
+          for(const action of actions) {
+            action(event)
+          }
+        };
+  
+        window.addEventListener(key, func);
+        EventsManager._EventsActionsMap.set(key, func);
+      }
+    }
+  }
+
+  public static AddEventMethod(eventId: EventsId, method: EventFunction) {
     const event = EventsManager._EventsMap.get(eventId);
+    
     if(!event) {
-      throw new Error(`Couldn't add method to ${eventId} event: is its listener set in EventsManager.ts ?`);
+      EventsManager.AddEventListener(eventId);
+      const event = EventsManager._EventsMap.get(eventId) as EventsSet;
+      event.add(method);
+
+      return;
     }
 
     event.add(method);
   }
 
-  public static RemoveEventMethod(eventId: EventsId, method: () => void) {
+  public static RemoveEventMethod(eventId: EventsId, method: EventFunction) {
     const event = EventsManager._EventsMap.get(eventId);
     if(!event) {
       throw new Error(`Couldn't find method to remove in EventsManager.ts`);
@@ -31,26 +50,24 @@ export class EventsManager {
     event.delete(method);
   }
 
-  private static _SetListeners() {
-    for(const [key, actions] of EventsManager._EventsMap) {
-      const func = (event: Event) => {
-        for(const action of actions) {
-          action(event)
-        }
-      }
-
-      window.addEventListener(key, func)
-      EventsManager._EventsActionsMap.set(key, func)
-    }
-  }
-
-  public static RemoveListener(listenerId: EventsId) {
+  public static RemoveEventListener(listenerId: EventsId) {
     const actions = EventsManager._EventsActionsMap.get(listenerId);
     
     if(!actions) {
-      throw new Error(`Couldn't find listener for ${listenerId} events to remove in EventManager.ts`)
+      throw new Error(`Couldn't find listener for ${listenerId} events to remove in EventManager.ts`);
     }
 
-    window.removeEventListener(listenerId, actions)
+    window.removeEventListener(listenerId, actions);
+    EventsManager._EventsActionsMap.delete(listenerId);
+    EventsManager._EventsMap.delete(listenerId);
+  }
+
+  public static AddEventListener(listenerId: EventsId) {
+    if(EventsManager._EventsMap.get(listenerId)) {
+      throw new Error(`${listenerId} listener already exists`);
+    };
+
+    EventsManager._EventsMap.set(listenerId, new Set<EventFunction>());
+    EventsManager._SetListeners();
   }
 }
